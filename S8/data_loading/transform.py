49db15e7fceba0b6f476531961555d7bb5a03de8
Rishabh Bhardwaj <rishabhbhardwaj64@gmail.com>
2020-09-14 13:33:36 +0530

    Update transform.py

--- S8/data_loading/transform.py
+++ S8/data_loading/transform.py
@@ -3,9 +3,9 @@ from torchvision import datasets, transforms
 def cifar10_transforms():
 
     train_transforms = transforms.Compose([
-        transforms.ToTensor(),
         transforms.RandomRotation((-10.0, 10.0), fill=(1,1,1)),
         transforms.RandomHorizontalFlip(p=0.2),
+        transforms.ToTensor(),
         transforms.Normalize((0.49139968, 0.48215841, 0.44653091), (0.24703223, 0.24348513, 0.26158784))
     ])
 
