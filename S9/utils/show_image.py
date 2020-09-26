a4fdd426653c117f8523eee9a5ebddd35bc637bc
Rishabh Bhardwaj <rishabhbhardwaj64@gmail.com>
2020-09-27 05:06:47 +0530

    Create show_image.py

---
+++ S9/utils/show_image.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import data_loading.transform as dt
+
+def imshow(img, mean, std):
+    un_norm = dt.UnNormalize(mean, std)
+    img = un_norm(img)
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+
+def plot_dataset_images(device, classes, data_loader, mean, std, num_of_images=10):
+    cnt = 0
+    fig = plt.figure(figsize=(10,10))
+    for data, target in data_loader:
+        # data, target = data.to(device), target.to(device)
+        for index, label in enumerate(target):
+            title = "{}".format(classes[label.item()])
+            
+            ax = fig.add_subplot(2, 5, cnt+1, xticks=[], yticks=[])
+            ax.set_title(title)
+            imshow(data[index].cpu(), mean, std)
+        
+            cnt += 1
+            if(cnt==num_of_images):
+                break
+        if(cnt==num_of_images):
+            break
