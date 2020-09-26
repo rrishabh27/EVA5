19f456514ca7b0e9d14466116b4401f3b1534d6a
Rishabh Bhardwaj <rishabhbhardwaj64@gmail.com>
2020-09-27 05:19:07 +0530

    Update show_image.py

--- S9/utils/show_image.py
+++ S9/utils/show_image.py
@@ -10,14 +10,17 @@ def imshow(img, mean, std):
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
 
 def plot_dataset_images(device, classes, data_loader, mean, std, num_of_images=10):
+    '''
+    plots train dataset images(max 20 images)
+    '''
     cnt = 0
-    fig = plt.figure(figsize=(10,10))
+    fig = plt.figure(figsize=(32,32))
     for data, target in data_loader:
         # data, target = data.to(device), target.to(device)
         for index, label in enumerate(target):
             title = "{}".format(classes[label.item()])
             
-            ax = fig.add_subplot(2, 5, cnt+1, xticks=[], yticks=[])
+            ax = fig.add_subplot(num_of_images/5 + 1, 5, cnt+1, xticks=[], yticks=[])
             ax.set_title(title)
             imshow(data[index].cpu(), mean, std)
         
