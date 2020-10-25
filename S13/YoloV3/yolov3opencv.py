6a2781b75c6460acc8471c8aebb9bac17d4e8b2f
Rishabh Bhardwaj <rishabhbhardwaj64@gmail.com>
2020-10-25 14:24:02 +0530

    Create yolov3opencv.py

---
+++ S13/YoloV3/yolov3opencv.py
@@ -0,0 +1,61 @@
+import cv2
+import numpy as np
+from google.colab.patches import cv2_imshow
+
+def yolo_opencv(path_weights, path_cfg, path_labels, path_img):
+    
+    # load yolo
+    net = cv2.dnn.readNet(path_weights, path_cfg)
+    classes = []
+    with open(path_labels, "r") as f:
+        classes = [line.strip() for line in f.readlines()]
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    colors = np.random.uniform(0, 255, size = (len(classes), 3))
+
+    # load image
+    img = cv2.imread(path_img)
+    img = cv2.resize(img, None, fx=0.4, fy=0.4)
+    height, width, channels = img.shape
+
+    # detecting objects
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    # Showing informations on the screen
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+    # output
+    font = cv2.FONT_HERSHEY_SIMPLEX 
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            color = colors[i]
+            cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(img, label, (x, y - 10), font, 1, color, 2)
+    cv2_imshow(img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
